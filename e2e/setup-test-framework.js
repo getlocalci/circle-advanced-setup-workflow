209231dcc0babcb84b22227ac8db7c09517bbf50
Ryan Kienstra <kienstraryan@gmail.com>
2022-01-06 19:08:44 -0600

    Remove calls of undefined functions

--- e2e/setup-test-framework.js
+++ e2e/setup-test-framework.js
@@ -1,5 +1,5 @@
 /**
- * Forked from Gutenberg, with only minor changes.
+ * Forked from Gutenberg, with with minor changes.
  *
  * @see https://github.com/WordPress/gutenberg/blob/df2b096185818f89b2c1668d22a848990acb2799/packages/e2e-tests/config/setup-test-framework.js
  */
@@ -12,34 +12,21 @@ import {
   setBrowserViewport,
 } from '@wordpress/e2e-test-utils';
 
-/**
- * Timeout, in seconds, that the test should be allowed to run.
- *
- * @type {string|undefined}
- */
-const PUPPETEER_TIMEOUT = process.env.PUPPETEER_TIMEOUT;
-
 // The Jest timeout is increased because these tests are a bit slow
-jest.setTimeout( PUPPETEER_TIMEOUT || 100000 );
+jest.setTimeout(process.env.PUPPETEER_TIMEOUT || 100000);
 
 async function setupBrowser() {
-  await setBrowserViewport( 'large' );
+  await setBrowserViewport('large');
 }
-
 
 // Before every test suite run, delete all content created by the test. This ensures
 // other posts/comments/etc. aren't dirtying tests and tests don't depend on
 // each other's side-effects.
 beforeAll( async () => {
-  capturePageEventsForTearDown();
   enablePageDialogAccept();
   await setupBrowser();
 } );
 
-afterEach( async () => {
+afterEach(async () => {
   await setupBrowser();
-} );
-
-afterAll( async () => {
-  removePageEvents();
-} );
+});
